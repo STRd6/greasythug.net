ba8daffe0f8e6e72a87242b73954025d5afb21a9
Daniel X Moore <yahivin@gmail.com>
2010-04-14 19:26:42 -0700

    Added json support for login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,22 +1,38 @@
-class UserSessionsController < ApplicationController
-  def new
-    @user_session = UserSession.new
-  end
+class UserSessionsController < ResourceController::Base
+  actions :new, :create, :destroy
+
+  skip_before_filter :verify_authenticity_token, :only => :create
 
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Successfully logged in."
-      redirect_to root_url
+      respond_to do |format|
+        format.html do
+          flash[:notice] = "Successfully logged in."
+          redirect_to root_url
+        end
+        format.json { render :json => current_user.to_json(:only => [:id, :display_name]) }
+      end
     else
-      render :action => 'new'
+      respond_to do |format|
+        format.html { render :action => 'new' }
+        format.json { render :json => @user_session.errors.to_json }
+      end
     end
   end
 
-  def destroy
-    @user_session = UserSession.find
-    @user_session.destroy
+  destroy.wants.html do
     flash[:notice] = "Successfully logged out."
     redirect_to root_url
+  end
+
+  private
+
+  def build_object
+    @object ||= UserSession.new
+  end
+
+  def object
+    @object ||= UserSession.find
   end
 end
